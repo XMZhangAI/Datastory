f75335efad185d235217a087f39f29a20fc221ea
k3sk1 <stiankk@protonmail.com>
2023-11-02 11:24:21 +0100

    Added energy data and done some small improvements

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, jsonify
-from .data_loading import wind_data, temp_data, co2_data, forest_data  # import data
+from .data_loading import energy_data, temp_data, co2_data, forest_data  # import data
 
 main_bp = Blueprint("main", __name__)
 
@@ -12,7 +12,7 @@ def home():
 @main_bp.route("/api/data/<string:data_type>")
 def get_data(data_type):
     data_dict = {
-        "energy": wind_data,
+        "energy": energy_data,
         "temperature": temp_data,
         "co2": co2_data,
         "forest": forest_data,
